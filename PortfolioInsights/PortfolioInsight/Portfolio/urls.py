bf3838827ab122d57814d09300e5e9d2102606b1
Amit Shrestha <amitshrestha@192.168.1.10>
2026-03-08 22:27:35 +0545

    Portfolio and Authentication Compeletd

---
+++ PortfolioInsights/PortfolioInsight/Portfolio/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path('loadmarket/', LoadMarketDataView.as_view()),
+    path('loaddata/', LoadMarketDataView, name='load_data'),
+    path('loadsample/', sample_view, name='loadsample'),
+    path("market/history/",MarketHistoryView.as_view(), name="marketHistory"),
+    path("market/latest/", MarketLatestView.as_view(), name="market-latest"),
+    path("market/series/",MarketSeriesView.as_view(),name="marketSeries"),
+    path("market/symbols/",MarketSymbolView.as_view(),name="marketSymbols"),
+    path("market/stocks/", MarketStocksView.as_view(), name="market-stocks"),
+    path("portfolios/", PortfolioListCreateView.as_view(), name="portfolio-list-create"),
+
+    path("stock-price/", StockPriceView.as_view(), name="stock-price"),
+    path("transactions/buy/", BuyTransactionView.as_view(), name="buy-transaction"),
+    path("transactions/", TransactionListView.as_view(), name="transaction-list"),
+
+
+
+]
